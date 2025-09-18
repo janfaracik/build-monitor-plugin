1c85fa727b5942bd45a8b9ec93af18c0ac42cc5f
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-09-18 09:06:46 +0100

    Add Slider test

--- src/main/frontend/components/label.tsx
+++ src/main/frontend/components/label.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from "react";
 
 function Label({ text, style = {} }: { text: string; style?: CSSProperties }) {
+  // The _intention_ is to have this component seamlessly transition between states, hence pulling it out into its own component
   return <span style={{ position: "relative", ...style }}>{text}</span>;
 }
 
